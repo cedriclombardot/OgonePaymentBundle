8428d78777bf9d258f9b2bde7658eeae983a8732
Cedric LOMBARDOT <cedric.lombardot@gmail.com>
2012-05-23 00:19:02 +0200

    Try fx tests

--- Tests/reset_tests.sh
+++ Tests/reset_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cp "$PWD/../Resources/config/schema.xml ./"
+cp $PWD/../Resources/config/schema.xml ./
 PHP_CLASSPATH="$PWD/../vendor/phing/phing/classes/" ../vendor/propel/propel1/generator/bin/propel-gen 
