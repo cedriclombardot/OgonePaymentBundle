7012a2532d38d77c96b38a2ac7d54c397c0bb1f7
Cedric LOMBARDOT <cedric.lombardot@gmail.com>
2012-10-22 16:07:32 +0200

    Fix chmod for travis

--- Tests/reset_tests.sh
+++ Tests/reset_tests.sh
@@ -3,4 +3,7 @@
 CURRENT=`pwd`
 cp $CURRENT/../Resources/config/schema.xml ./
 chmod +x "$CURRENT/../vendor/phing/phing/bin/phing"
+
+chmod +x ../vendor/propel/propel1/generator/bin/phing.php
+
 PHING_COMMAND="$CURRENT/../vendor/phing/phing/bin/phing" PHP_CLASSPATH="$CURRENT/../vendor/phing/phing/classes/" ../vendor/propel/propel1/generator/bin/propel-gen
